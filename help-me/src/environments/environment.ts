38e09e2265256f03f9c388f27f8385ce04c8867b
Vishnu Thankappan <vishnunadaar111@gmail.com>
2024-11-18 18:43:57 +0530

    Update environment.ts

--- help-me/src/environments/environment.ts
+++ help-me/src/environments/environment.ts
@@ -3,24 +3,25 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  MAIN_URL:"http://localhost:8100",
-  Server_URL:'http://localhost:3000/',
-  LOGIN_REDIRECT:'http://localhost:8100/home',
+  production: true,
+  MAIN_URL:"https://helpgenie.in",
+  Server_URL:'https://help-genieapi.onrender.com/',
+  LOGIN_REDIRECT:'https://helpgenie.in/home',
   firebaseConfig : {
-    apiKey: "AIzaSyDJGfrQGJeQyLR-CP7iwLweqv4PmEyBgTc",
-    authDomain: "helpgenie-15944.firebaseapp.com",
-    projectId: "helpgenie-15944",
-    storageBucket: "helpgenie-15944.appspot.com",
-    messagingSenderId: "437281664939",
-    appId: "1:437281664939:web:889b6402360c00082e6cee",
+    apiKey: "AIzaSyDJGfrQGJeQyLsdR-CP7iwLweqv4PmEyBgTc",
+    authDomain: "helpgenie-1532944.firebaseapp.com",
+    projectId: "helpgenie-1fas593234",
+    storageBucket: "helpgenie-ds.appspot.com",
+    messagingSenderId: "4372816632939",
+    appId: "1:437281664939:web:889b6402das360c00082e6cee",
     measurementId: "G-MKNKYCDMG3"
   },
-  ONESIGNAL_APP_ID:"139af684-e9c9-4018-9fd3-4e6cbc7af8ad",
-  GMAP_API_KEY:"AIzaSyCRn1s6OnCG3P_fBK2-ofohuiLXRYOWxAw",
+  ONESIGNAL_APP_ID:"139af684-e9c9-423as18-9fd3-4e6cbc7af8ad",
+  GMAP_API_KEY:"AIzaSy32CRn1s6OnCG3P_fBK2-ofohuiLXRYOWxAw",
   help_mail:'helpgenieservice@gmail.com',
   necktar_mail:'necktar.life@gmail.com',
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
